ab60c65a2611bd8e308c36be1e5de9ca3b93c8c5
root <root@LAPTOP-SCVRHSJC>
2024-09-20 10:26:28 +0800

    ALU with complete tests Finished!

--- ALU/ALU32bits/sim_main.cpp
+++ ALU/ALU32bits/sim_main.cpp
@@ -9,12 +9,48 @@ int main(int argc, char** argv) {
     VerilatedContext* contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     VALU32bits* top = new VALU32bits{contextp};  // 修正了初始化错误
+    
+    for(int i = 0; i <= 10; i++){
+    	top->A = i;
+    	top->B = 2 * i - 30;
+    	top->FSel = 0b000;
+    	top->eval();
+	int expected_output = i + (2 * i - 30) & 0xFFFFFFFF; 
+	assert(((top->Output & 0xFFFFFFFF) == expected_output) && "Sum mismatch");
+    	std::cout<<"result = "<<(int)(top->Output)<<std::endl; 
+    }
+    
 
-    top->A = 41;
-    top->B = 41;
-    top->FSel = 0b111;
-    top->eval();
-    std::cout<<"result = "<<(int)(top->Output & 0b1)<<std::endl; 
+    for(int i = 0; i <= 10; i++){
+    	top->A = i;
+    	top->B = 2 * i - 30;
+    	top->FSel = 0b001;
+    	top->eval();
+	int expected_output = i - (2 * i - 30) & 0xFFFFFFFF; 
+	assert(((top->Output & 0xFFFFFFFF) == expected_output) && "Sub mismatch");
+    	std::cout<<"result = "<<(int)(top->Output)<<std::endl; 
+    }
+
+
+    for(int i = 0; i <= 10; i++){
+    	top->A = i;
+    	top->B = 2 * i + 30;
+    	top->FSel = 0b110;
+    	top->eval();
+	assert(((top->Output & 0b1) == 0b1) && "Less mismatch");
+    	std::cout<<"result = "<<(int)(top->Output)<<std::endl; 
+    }
+
+
+
+    for(int i = 0; i <= 10; i++){
+    	top->A = i;
+    	top->B = i;
+    	top->FSel = 0b111;
+    	top->eval();
+	assert(((top->Output & 0b1) == 0b1) && "Equal mismatch");
+    	std::cout<<"result = "<<(int)(top->Output)<<std::endl; 
+    }
     printf("All tests passed!\n");
 
     // 释放资源
